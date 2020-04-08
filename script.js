4a80ed70088e9b836869e4556869f77c00f683ec
Kristina <krdiamond@gmail.com>
2020-04-08 17:23:33 -0400

    this is done!

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
+document.getElementById('12').addEventListener("click", function() {
+    document.getElementById("text").style.display='block';
+    document.getElementById("text").innerHTML = "<p>Very cold... brrr</p>"
+  });
+
 document.getElementById('28').addEventListener("click", function() {
+    document.getElementById("text").style.display='block';
+    document.getElementById("text").innerHTML = "<p>That would be too easy lol</p>"
+  });
+
+  document.getElementById('20').addEventListener("click", function() {
+    document.getElementById("text").style.display='block';
+    document.getElementById("text").innerHTML = "<p>Getting warmer... </p>"
+  });
+
+  document.getElementById('42').addEventListener("click", function() {
+    document.getElementById("text").style.display='block';
+    document.getElementById("text").innerHTML = "<p>nothings here </p>"
+  });
+
+  document.getElementById('22').addEventListener("click", function() {
     document.getElementById("text").style.display='block';
     document.getElementById("text").innerHTML = "<p>That would be too easy lol</p>"
   });
 
   document.getElementById('25').addEventListener("click", function() {
     document.getElementById("text").style.display='block';
-    document.getElementById("text").innerHTML = "<p>Getting warmer...</p>"
+    document.getElementById("text").innerHTML = "<p>hot hot hot  !</p>"
   });
 
   document.getElementById('33').addEventListener("click", function() {
     document.getElementById("text").style.display='block';
-    document.getElementById("text").innerHTML = "<p>I know anything could be true in the digital world but Matza is still too big to fit under a remote...</p>"
+    document.getElementById("text").innerHTML = "<p> Matzo is too big to fit under a remote...</p>"
   });
 
   document.getElementById('37').addEventListener("click", function() {
@@ -18,13 +38,13 @@ document.getElementById('28').addEventListener("click", function() {
     document.getElementById("text").innerHTML = "<p>nope</p>"
   });
 
-  document.getElementById('44').addEventListener("click", function() {
+  document.getElementById('38').addEventListener("click", function() {
     document.getElementById("text").style.display='block';
-    document.getElementById("matza").style.display='block';
+    document.getElementById("matzo").style.display='block';
     let time = new Date();
     let yourTime = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
 
-    document.getElementById("text").innerHTML = "<p>YOU FOUND IT!!!!!!!!!!!!!!!!!</p><p>" +yourTime+ "</p>";
+    document.getElementById("text").innerHTML = "<p>YOU FOUND THE AFIKOMEN !!!!!!!!!!!!!!!!!</p><p>" +yourTime+ "</p>";
   });
 
 
